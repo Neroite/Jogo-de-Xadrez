f87c67595611115d71705d4ae91e6ff82fbd9724
Nathan Rocha <neroite71@hotmail.com>
2023-08-22 10:07:12 -0300

    Testes iniciais com movimentos possiveis das pecas

--- Xadrez-Console/Tabuleiro/Peca.cs
+++ Xadrez-Console/Tabuleiro/Peca.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Tabuleiro
 {
-    internal class Peca
+    internal abstract class Peca
     {
         public Posicao Posicao { get; set; }
         public Cor Cor { get; protected set; }
@@ -21,5 +21,6 @@ namespace Tabuleiro
             QteMovimentos++;
         }
 
+        public abstract bool[,] MovimentosPossiveis();
     }
 }
